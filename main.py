b910a5263c6434d73d5cdb4571f8b314732ec740
Max Wang <mwang0712@gmail.com>
2017-04-05 01:32:00 -0700

    a new feature with counter

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 
 # say hello to the world
 
-def say_hello():
-    print "hello, world"
-    print "this is a test"
+def say_hello(num):
+    for i in range(num):
+        print "this is a new message", i
 
 
 for i in range(3):
-    say_hello()
+    say_hello(4)
 
 # history
 # we add this history
