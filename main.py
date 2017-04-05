7229860557c24f8f778658554ec56bc15e43a381
Max Wang <mwang0712@gmail.com>
2017-04-05 01:43:07 -0700

    cool title

--- main.py
+++ main.py
@@ -2,9 +2,10 @@
 # say hello to the world
 
 def say_hello(num):
+    print "=====STARTING====="
     for i in range(num):
         print "this is a super message", i
-
+        
 
 for i in range(2):
     say_hello(3)
